6328b6b0d5f3f9eb81f2a0c975326b0af410ea80
tianhl <tianhl@ihep.ac.cn>
2016-12-14 12:55:32 +0800

    upload setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+export CMAKE_INCLUDE_PATH=$C_INCLUDE_PATH:$CPLUS_INCLUDE_PATH:$CMAKE_INCLUDE_PATH
+export CMAKE_LIBRARY_PATH=$LD_LIBRARY_PATH:$CMAKE_LIBRARY_PATH
+
+export CMAKE_INCLUDE_PATH=/home/tianhl/workarea/Codes/GPPD_CODE/sniper/SniperInstall/include:$CMAKE_INCLUDE_PATH
+export CMAKE_LIBRARY_PATH=/home/tianhl/workarea/Codes/GPPD_CODE/sniper/SniperInstall/lib:$CMAKE_LIBRARY_PATH
+export CMAKE_LIBRARY_PATH=/home/tianhl/workarea/Codes/GPPD_CODE/sniper/SniperInstall/pylib:$CMAKE_LIBRARY_PATH
+export    LD_LIBRARY_PATH=/home/tianhl/workarea/Codes/GPPD_CODE/sniper/SniperInstall/lib:$LD_LIBRARY_PATH
+export         PYTHONPATH=/home/tianhl/workarea/Codes/GPPD_CODE/sniper/SniperInstall/pylib:$PYTHONPATH
+export  CMAKE_MODULE_PATH=/home/tianhl/workarea/Codes/GPPD_CODE/sniper/CMake:$CMAKE_MODULE_PATH
+
+export CMAKE_INCLUDE_PATH=/opt/dim/dim_v20r17/dim:$CMAKE_INCLUDE_PATH
+export CMAKE_LIBRARY_PATH=/opt/dim/dim_v20r17/linux:$CMAKE_LIBRARY_PATH
+
+export LD_LIBRARY_PATH=$CMAKE_LIBRARY_PATH:$LD_LIBRARY_PATH
+
