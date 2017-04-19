745c50d6e26ff128c4a51322008228df4783e03f
tiamhl <tiamhl@ihep.ac.cn>
2017-04-19 10:56:35 +0800

    remove dim

--- setup.sh
+++ setup.sh
@@ -6,10 +6,10 @@ export CMAKE_LIBRARY_PATH=/opt/sniper-1.0/SniperInstall/lib:$CMAKE_LIBRARY_PATH
 export CMAKE_LIBRARY_PATH=/opt/sniper-1.0/SniperInstall/pylib:$CMAKE_LIBRARY_PATH
 export    LD_LIBRARY_PATH=/opt/sniper-1.0/SniperInstall/lib:$LD_LIBRARY_PATH
 export         PYTHONPATH=/opt/sniper-1.0/SniperInstall/pylib:$PYTHONPATH
-export  CMAKE_MODULE_PATH=/home/tianhl/workarea/Codes/GPPD_CODE/sniper/CMake:$CMAKE_MODULE_PATH
+export  CMAKE_MODULE_PATH=/home/tianhl/workarea/Codes/DroNE/CMake:$CMAKE_MODULE_PATH
 
-export CMAKE_INCLUDE_PATH=/opt/dim/dim_v20r17/dim:$CMAKE_INCLUDE_PATH
-export CMAKE_LIBRARY_PATH=/opt/dim/dim_v20r17/linux:$CMAKE_LIBRARY_PATH
+#export CMAKE_INCLUDE_PATH=/opt/dim/dim_v20r17/dim:$CMAKE_INCLUDE_PATH
+#export CMAKE_LIBRARY_PATH=/opt/dim/dim_v20r17/linux:$CMAKE_LIBRARY_PATH
 
 export LD_LIBRARY_PATH=$CMAKE_LIBRARY_PATH:$LD_LIBRARY_PATH
 
