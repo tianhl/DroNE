bdcb3fcd72f0023b2b01718f784581f64f4d722b
tiamhl <tiamhl@ihep.ac.cn>
2017-05-22 13:22:23 +0800

    DroNECore Python package

---
+++ DroNECore/python/DroNECore/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# author: tianhl
+# python binding for DataSvc
+
+import Sniper
+from libDroNECorePython import *
+from PyIncident import *
+from CtrlTask import *
