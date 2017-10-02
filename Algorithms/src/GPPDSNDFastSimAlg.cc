7ec0ba261ed2c4fde14333891a25289d4e966985
tiamhl <tiamhl@ihep.ac.cn>
2017-10-02 17:37:32 +0800

    hit simulation of GPPDSNDFastSimAlg

--- Algorithms/src/GPPDSNDFastSimAlg.cc
+++ Algorithms/src/GPPDSNDFastSimAlg.cc
@@ -58,24 +58,24 @@ bool
 GPPDSNDFastSimAlg::execute()
 {
     ++m_count;
-
+    m_hitcol->clear();
     uint32_t size = m_evtcol->size();
-    LogInfo << m_count << " pulse, including " << size << " evts needed to be simulated" << std::endl; 
 
     for(uint32_t i = 0; i < size; i++){
 	    Evt* evt = m_evtcol->at(i);
 	    uint32_t tof = evt->getTOF();
 	    uint32_t pid = evt->getPixelID();
-            uint32_t tdata = tof*80;
-            uint32_t xchan = 0;
-            uint32_t ychan = 0;
-       
-            getXYChannel(pid, xchan, ychan);
-            //std::cout << "FastSim pid: " << pid << " tof: " << tof << " x channel: " << xchan
-            //          << " y channel: " << ychan << std::endl;
-            simulation(xchan, ychan, tdata);
+	    uint32_t tdata = tof*10; // 1us => 0.1us
+	    uint32_t xchan = 0;
+	    uint32_t ychan = 0;
+
+	    getXYChannel(pid, xchan, ychan);
+	    simulation(xchan, ychan, tdata);
+	    //std::cout << "FastSim pid: " << pid << " tof: " << tof << " x channel: " << xchan
+	    //          << " y channel: " << ychan << std::endl;
     }
 
+    LogInfo << m_count << " pulse, including " << size << " evts " << m_hitcol->size() << " hits" << std::endl; 
     return true;
 }
 
@@ -90,7 +90,27 @@ void GPPDSNDFastSimAlg::getXYChannel(uint32_t pid, uint32_t& xchannel, uint32_t&
 }
 
 void GPPDSNDFastSimAlg::simulation(uint32_t xchan, uint32_t ychan, uint32_t tdata){
-	m_hitcol->clear();
+	int position;
+	uint32_t xsize = uint32_t(rand()%5);
+	for(uint32_t i = 0; i < xsize; i++){
+		position = xchan + i;
+		if(position<=111)calculation(position, tdata);
+		position = xchan - i;
+		if(position>=1  )calculation(position, tdata);
+	}
+	uint32_t ysize = uint32_t(rand()%5);
+	for(uint32_t i = 0; i < ysize; i++){
+		position = 127 + ychan + i;
+		if(position<=175)calculation(position, tdata);
+		position = 127 + ychan - i;
+		if(position>=128)calculation(position, tdata);
+	}
+}
 
+void GPPDSNDFastSimAlg::calculation(uint32_t position, uint32_t timedata){
+	uint32_t time_offset = uint32_t(rand()%70);
+	Hit* hit = m_hitcol->add_item();
+	hit->setChannel(position);
+	hit->setTOF(timedata+time_offset);
 }
 
